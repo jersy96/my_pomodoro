5d8d12c184624d3d9611d0300c2263fb57c47288
Javier Roncallo <jroncallo96@gmail.com>
2020-07-20 12:41:08 -0500

    Create the pomodoro widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,26 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My pomodoro',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'My pomodoro'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +29,169 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Pomodoro(),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
+class Pomodoro extends StatefulWidget {
+  Pomodoro({Key key}) : super(key: key);
+
+  @override
+  _PomodoroState createState() => _PomodoroState();
+}
+
+class _PomodoroState extends State<Pomodoro> {
+  static final TextStyle _timeTextStyle =
+      TextStyle(color: Colors.black, fontSize: 30);
+
+  List<int> _durationOptions;
+  String _textFieldValue;
+  Duration _tick;
+  String _displayTime;
+  Timer _currentTimer;
+  int _currentDurationIndex;
+  Duration _currentCountdown;
+
+  @override
+  void initState() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _tick = Duration(milliseconds: 100);
+      _displayTime = '00:00';
+      _currentDurationIndex = 0;
+      _durationOptions = <int>[];
     });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: <Widget>[
+        SizedBox(
+          height: 400,
+          width: 400,
+          child: CircularProgressIndicator(
+            value: _calculateProgress(),
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.lime),
+          ),
+        ),
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  _displayTime,
+                  style: _timeTextStyle,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                  width: 400,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        hintText: 'Escribe los intervalos separados por coma'),
+                    onChanged: (text) {
+                      _textFieldValue = text;
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  child: Text('Iniciar'),
+                  shape: StadiumBorder(),
+                  onPressed: () => onStartButtonPressed(context),
+                )
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
     );
+  }
+
+  void onStartButtonPressed(BuildContext context) {
+    _durationOptions = getDurationOptionsFromField();
+    startNextTimer();
+  }
+
+  void startTimer(Duration duration) {
+    DateTime endTime = DateTime.now().add(duration);
+    _currentTimer = Timer.periodic(_tick, (Timer timer) {
+      setState(() {
+        _currentCountdown = endTime.difference(DateTime.now());
+        _displayTime = _generateDisplayTime(_currentCountdown);
+        if (DateTime.now().isAfter(endTime)) {
+          stopTimer();
+          startNextTimer();
+        }
+      });
+    });
+  }
+
+  void stopTimer() => setState(() {
+        _currentTimer.cancel();
+      });
+
+  List<int> getDurationOptionsFromField() {
+    List<String> durationOptionStrings = _textFieldValue?.split(',');
+    return durationOptionStrings?.map(int.parse)?.toList();
+  }
+
+  String _generateDisplayTime(Duration time) {
+    int minutes = time.inMinutes;
+    int seconds = (time.inSeconds - (time.inMinutes * 60));
+    return '$minutes:${seconds.toString().padLeft(2, '0')}';
+  }
+
+  double _calculateProgress() {
+    Duration duration = getCurrentDuration();
+    if (duration == null) {
+      return 1;
+    }
+    return _currentCountdown.inMilliseconds / duration.inMilliseconds;
+  }
+
+  void startNextTimer() {
+    _currentDurationIndex =
+        (_currentDurationIndex + 1) % _durationOptions.length;
+    startTimer(getCurrentDuration());
+  }
+
+  Duration getCurrentDuration() {
+    if (_durationOptions.isEmpty) {
+      return null;
+    }
+    int durationValue = _durationOptions[_currentDurationIndex];
+    return Duration(minutes: durationValue);
   }
 }
